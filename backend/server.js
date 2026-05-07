31b00b1d4369ab493746669b38218e8f7a233450
nandita-rana-09 <nanditarana007@gmail.com>
2026-05-07 23:03:28 +0530

    Update server.js
    Updated the frontedn URL

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // frontend URL
+    origin: "https://community-page-mern-iu6d.vercel.app/", // frontend URL
     methods: ["GET", "POST"]
   }
 });
